5d3fe19fecb4b3d64ff55ecbd5fcbcc31bed5a18
Izdihar <ezdiharomerrrr@gmail.com>
2023-04-25 12:39:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    const hq = 42;
+    return Math.abs(hq - street);
+  }
+
+  function distanceFromHqInFeet(street) {
+    const blockInFeet = 264;
+    return Math.abs(street - 42) * blockInFeet;
+  }
+  function distanceTravelledInFeet(start, end) {
+    const blockInFeet = 264;
+    const distanceInBlocks = Math.abs(end - start);
+    return distanceInBlocks * blockInFeet;
+  }
+  function calculatesFarePrice(start, destination) {
+    const blockInFeet = 264;
+    const freeDistance = 400;
+    const distanceInFeet = Math.abs(destination - start) * blockInFeet;
+  
+    if (distanceInFeet <= freeDistance) {
+      return 0;
+    } else if (distanceInFeet <= 2000) {
+      const farePrice = (distanceInFeet - freeDistance) * 0.02;
+      return parseFloat(farePrice.toFixed(2));
+    } else if (distanceInFeet <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
